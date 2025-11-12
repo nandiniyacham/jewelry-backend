b539cd4eb15289cc98f4bc9d9bd3b4908bacb013
Nandini yacham <nandiniyacham@gmail.com>
2025-11-12 23:39:20 +0530

    Fix API URL routing

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CategoryViewSet, products_by_category, register_user
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from django.urls import path
+from . import views
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
 router.register('categories', CategoryViewSet, basename='category')
@@ -13,4 +14,8 @@ urlpatterns = [
     path('users/register/', register_user),
     path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('products/', views.ProductListCreateView.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/products/', views.ProductsByCategoryView.as_view(), name='products-by-category'),
 ]
